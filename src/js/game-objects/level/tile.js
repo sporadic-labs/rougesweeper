54898be1087166145df5be4b580483f6df6bf0cb
Michael Hadley <michaelwesthadley@gmail.com>
2019-03-07 07:12:21 -0600

    Add some hover styles to Tile

--- src/js/game-objects/level/tile.js
+++ src/js/game-objects/level/tile.js
@@ -10,6 +10,8 @@ const TYPE_TO_KEY = {
 export default class Tile {
   /** @param {Phaser.Scene} scene */
   constructor(scene, type, x, y) {
+    this.scene = scene;
+
     this.backSprite = scene.add.sprite(0, 0, "assets", `tiles/tile-back`);
     this.frontSprite = scene.add.sprite(0, 0, "assets", `tiles/${TYPE_TO_KEY[type]}`);
     this.container = scene.add.container(x, y, [this.backSprite, this.frontSprite]);
@@ -19,19 +21,41 @@ export default class Tile {
     // TESTING
     this.container.setSize(this.backSprite.width, this.backSprite.height);
     this.container.setInteractive();
-    this.container.on("pointerover", this.flipToFront);
-    this.container.on("pointerout", this.flipToBack);
+    this.container.on("pointerover", this.onHoverStart);
+    this.container.on("pointerout", this.onHoverEnd);
   }
 
-  flipToFront = () => {
+  onHoverStart = () => {
+    this.flipToFront();
+    if (this.tween) this.tween.stop();
+    this.tween = this.scene.add.tween({
+      targets: this.container,
+      scaleX: 1.1,
+      scaleY: 1.1,
+      duration: 100
+    });
+  };
+
+  onHoverEnd = () => {
+    this.flipToBack();
+    if (this.tween) this.tween.stop();
+    this.tween = this.scene.add.tween({
+      targets: this.container,
+      scaleX: 1,
+      scaleY: 1,
+      duration: 100
+    });
+  };
+
+  flipToFront() {
     this.isFrontVisible = true;
     this.backSprite.setVisible(!this.isFrontVisible);
     this.frontSprite.setVisible(this.isFrontVisible);
-  };
+  }
 
-  flipToBack = () => {
+  flipToBack() {
     this.isFrontVisible = false;
     this.backSprite.setVisible(!this.isFrontVisible);
     this.frontSprite.setVisible(this.isFrontVisible);
-  };
+  }
 }
