f2275ebc7f87534f7f1df57dbb9e289690e75279
rex <retwedt@gmail.com>
2019-03-08 00:17:47 -0600

    Player knows what position it is in the grid.  Player keeps track of it's own health and gold.

--- src/js/game-objects/player/index.js
+++ src/js/game-objects/player/index.js
@@ -2,10 +2,61 @@ export default class Player {
   /** @param {Phaser.Scene} scene */
   constructor(scene, x, y) {
     this.sprite = scene.add.sprite(x, y, "assets", "player");
+
+    this.gridX = 0;
+    this.gridY = 0;
+
+    this.playerHealth = 3;
+    this.purse = 0;
   }
 
   setPosition(x, y) {
     this.sprite.setPosition(x, y);
+  }
+
+  getPosition() {
+    return { x: this.sprite.x, y: this.sprite.y };
+  }
+
+  setGridPosition(x, y) {
+    this.gridX = x;
+    this.gridY = y;
+  }
+
+  getGridPosition() {
+    return { x: this.gridX, y: this.gridY };
+  }
+
+  addHealth() {
+    if (this.playerHealth < 3) {
+      this.playerHealth++;
+      console.log(`New Player health: ${this.playerHealth}`);
+    } else {
+      console.log(`Max health: ${this.playerHealth}!`);
+    }
+  }
+
+  removeHealth() {
+    if (this.playerHealth <= 1) {
+      this.playerHealth = 0;
+      console.log("Game Over, Man!");
+      // TODO(rex): Actually end the game...
+    } else if (this.playerHealth > 1) {
+      this.playerHealth--;
+      console.log(`New Player health: ${this.playerHealth}!`);
+    }
+  }
+
+  addGold() {
+    this.purse++;
+    console.log(`Purse: ${this.purse} gold!`);
+  }
+
+  removeGold() {
+    if (this.purse > 0) {
+      this.purse--;
+    }
+    console.log(`Purse: ${this.purse} gold!`);
   }
 
   destroy() {
