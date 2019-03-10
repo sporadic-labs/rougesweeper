a71e1a469a9c9f439df30c76606c540d3c32ba19
rex <retwedt@gmail.com>
2019-03-10 00:11:22 -0600

    Attack animation for the player.

---
+++ src/js/game-objects/player/attack-animation.js
@@ -0,0 +1,46 @@
+export default class PlayerAttackAnimation {
+  /**
+   * @param {Phaser.Scene} scene
+   * @param {number} x
+   * @param {number} y
+   * @param {Function} cb
+   */
+  constructor(scene, x, y) {
+    this.scene = scene;
+
+    this.sprite = scene.add
+      .sprite(0, 0, "assets", "attack")
+      .setOrigin(0.5, 0.5);
+    this.sprite.setVisible(false);
+
+    this.setPosition(x, y);
+  }
+
+  fadeout(delay = 0) {
+    setTimeout(() => {
+      this.sprite.setVisible(true);
+    }, delay);
+    return new Promise((resolve, reject) => {
+      this.scene.tweens.add({
+        targets: this.sprite,
+        alpha: 0,
+        duration: 600,
+        delay: delay,
+        ease: "Quad.easeOut",
+        onComplete: resolve,
+        callbackScope: this
+      });
+    });
+  }
+
+  setPosition(x, y) {
+    const cx = x + this.sprite.width / 2;
+    this.sprite.x = cx;
+    this.sprite.y = y;
+  }
+
+  destroy() {
+    this.scene.tweens.killTweensOf(this.text);
+    this.sprite.destroy();
+  }
+}
