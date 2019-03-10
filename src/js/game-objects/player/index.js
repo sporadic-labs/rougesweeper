23eb0b548b48f2861219c1dbadcb5220af62e215
rex <retwedt@gmail.com>
2019-03-09 22:12:02 -0600

    Remove health and gold values from the player, they are going to the global store.

--- src/js/game-objects/player/index.js
+++ src/js/game-objects/player/index.js
@@ -8,9 +8,6 @@ export default class Player {
 
     this.gridX = 0;
     this.gridY = 0;
-
-    this.playerHealth = 3;
-    this.purse = 0;
   }
 
   setPosition(x, y) {
@@ -28,38 +25,6 @@ export default class Player {
 
   getGridPosition() {
     return { x: this.gridX, y: this.gridY };
-  }
-
-  addHealth() {
-    if (this.playerHealth < 3) {
-      this.playerHealth++;
-      console.log(`New Player health: ${this.playerHealth}`);
-    } else {
-      console.log(`Max health: ${this.playerHealth}!`);
-    }
-  }
-
-  removeHealth() {
-    if (this.playerHealth <= 1) {
-      this.playerHealth = 0;
-      console.log("Game Over, Man!");
-      // TODO(rex): Actually end the game...
-    } else if (this.playerHealth > 1) {
-      this.playerHealth--;
-      console.log(`New Player health: ${this.playerHealth}!`);
-    }
-  }
-
-  addGold() {
-    this.purse++;
-    console.log(`Purse: ${this.purse} gold!`);
-  }
-
-  removeGold() {
-    if (this.purse > 0) {
-      this.purse--;
-    }
-    console.log(`Purse: ${this.purse} gold!`);
   }
 
   destroy() {
