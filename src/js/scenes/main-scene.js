efb2c52e30e9fefd86dff8a9de1e4f9245c0469f
rex <retwedt@gmail.com>
2019-03-09 22:16:31 -0600

    Add the player health indicator and gold indicator to the scene.

--- src/js/scenes/main-scene.js
+++ src/js/scenes/main-scene.js
@@ -3,6 +3,8 @@ import Level from "../game-objects/level/level";
 import Player from "../game-objects/player/index";
 import GameManager from "../game-objects/game-manager/index";
 import DangerIndicator from "../game-objects/danger-indicator";
+import PurseIndicator from "../game-objects/purse-indicator";
+import HealthIndicator from "../game-objects/health-indicator";
 import store from "../store/index";
 
 export default class Scene extends Phaser.Scene {
@@ -15,5 +17,7 @@ export default class Scene extends Phaser.Scene {
     const gameManager = new GameManager(this, player);
 
     new DangerIndicator(this, store);
+    new PurseIndicator(this, store);
+    new HealthIndicator(this, store);
   }
 }
