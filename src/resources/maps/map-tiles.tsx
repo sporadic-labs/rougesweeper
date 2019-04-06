2207c67e774b9c98a2cd3b905cfb0b2f4ba8ed09
rex <retwedt@gmail.com>
2019-04-06 15:14:54 -0500

    Add temp tiled maps for trying out walls.

---
+++ src/resources/maps/map-tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="map-tiles" tilewidth="76" tileheight="76" tilecount="3" columns="3">
+ <image source="map-tiles.png" width="228" height="76"/>
+</tileset>
