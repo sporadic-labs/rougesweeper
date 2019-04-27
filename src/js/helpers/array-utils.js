c579ed628d6ba8c691ad1cbf5338aa9ebadf0836
Michael Hadley <michaelwesthadley@gmail.com>
2019-04-27 10:08:41 -0500

    Convert array-utils to TS

--- src/js/helpers/array-utils.js
+++
@@ -1,7 +0,0 @@
-export function arrayEqual(array1, array2) {
-  return array1.length === array2.length && array1.every((elem, index) => elem === array2[index]);
-}
-
-export function create2DArray(width, height, value) {
-  return [...Array(height)].map(() => Array(width).fill(value));
-}
---
+++ src/js/helpers/array-utils.ts
@@ -0,0 +1,13 @@
+/**
+ * Check if two arrays are equal by doing a shallow element-by-element comparison.
+ */
+export function arrayEqual(array1: any[], array2: any[]): Boolean {
+  return array1.length === array2.length && array1.every((elem, index) => elem === array2[index]);
+}
+
+/**
+ * Creates a 2D array of width x height, filled with the given value.
+ */
+export function create2DArray(width: number, height: number, value: any): any[][] {
+  return [...Array(height)].map(() => Array(width).fill(value));
+}
