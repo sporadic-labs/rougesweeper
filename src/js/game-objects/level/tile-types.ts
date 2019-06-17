d2aaa2147f7775dfc912720eac07157aa4a59a65
Michael Hadley <michaelwesthadley@gmail.com>
2019-06-17 09:54:23 -0500

    TILE_TYPES to TS enum

---
+++ src/js/game-objects/level/tile-types.ts
@@ -0,0 +1,12 @@
+enum TILE_TYPES {
+  START = "START",
+  EXIT = "EXIT",
+  BLANK = "BLANK",
+  ENEMY = "ENEMY",
+  GOLD = "GOLD",
+  SHOP = "SHOP",
+  WALL = "WALL",
+  KEY = "KEY"
+}
+
+export default TILE_TYPES;
