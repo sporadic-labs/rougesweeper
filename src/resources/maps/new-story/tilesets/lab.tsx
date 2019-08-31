3a16aa3992456018443bd3295c63406fa03eed25
rex <retwedt@gmail.com>
2019-08-31 10:17:54 -0500

    Tilesets for tiled.

---
+++ src/resources/maps/new-story/tilesets/lab.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="lab" tilewidth="38" tileheight="38" tilecount="100" columns="10">
+ <image source="lab.png" width="380" height="380"/>
+</tileset>
---
+++ src/resources/maps/new-story/tilesets/warehouse.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="warehouse" tilewidth="38" tileheight="38" tilecount="100" columns="10">
+ <image source="warehouse.png" width="380" height="380"/>
+</tileset>
