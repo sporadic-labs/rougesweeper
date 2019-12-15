3461ae81376262086e6843ebd21ee10d65beb696
Michael Hadley <michaelwesthadley@gmail.com>
2019-12-15 15:37:19 -0600

    Bug: tweens don't have a destroy method

--- src/js/game-objects/hud/radar.ts
+++ src/js/game-objects/hud/radar.ts
@@ -228,7 +228,7 @@ class Radar {
   }
 
   destroy() {
-    if (this.labelTween) this.labelTween.destroy();
+    if (this.labelTween) this.labelTween.stop();
     if (this.scrambleDangerCountTimer) this.scrambleDangerCountTimer.remove(false);
     this.scene = undefined;
     this.outlineGraphics.destroy();
