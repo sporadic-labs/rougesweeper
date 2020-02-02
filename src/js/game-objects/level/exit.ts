59b2fe5aac67016bbcbc8eea90cf8f56cd8f1d45
Michael Hadley <michaelwesthadley@gmail.com>
2020-02-02 15:08:06 -0600

    Update Exit to have a tile underneath it

--- src/js/game-objects/level/exit.ts
+++ src/js/game-objects/level/exit.ts
@@ -1,10 +1,17 @@
 import { Scene, Events, GameObjects } from "phaser";
 import EVENTS from "./events";
 import DEPTHS from "../depths";
+import FlipEffect from "../components/flip-effect";
+import { MagnifyEffect } from "../components/magnify-effect";
 
 export default class Exit {
-  private sprite: GameObjects.Sprite;
-  private tween: Phaser.Tweens.Tween;
+  private doorSprite: GameObjects.Sprite;
+  private doorTween: Phaser.Tweens.Tween;
+  private tileTween: Phaser.Tweens.Tween;
+  private tileContainer: Phaser.GameObjects.Container;
+  private tileFlipEffect: FlipEffect;
+  private doorMagnifyEffect: MagnifyEffect;
+  private tileMagnifyEffect: MagnifyEffect;
 
   constructor(
     private scene: Scene,
@@ -12,13 +19,21 @@ export default class Exit {
     worldY: number,
     private levelEvents: Events.EventEmitter,
     private doorPrefix: string,
-    private isCurrentlyOpen = false
+    private isCurrentlyOpen = false,
+    tileKey: string
   ) {
-    this.sprite = scene.add
-      .sprite(0, 0, "all-assets", `${doorPrefix}-0`)
+    const tileBack = scene.add.sprite(0, 0, "all-assets", "tile-back");
+    const tileFront = scene.add.sprite(0, 0, "all-assets", tileKey);
+    this.tileFlipEffect = new FlipEffect(scene, tileFront, tileBack);
+    this.tileFlipEffect.setToBack();
+    this.tileContainer = scene.add.container(worldX, worldY, [tileBack, tileFront]);
+    this.tileContainer.setDepth(DEPTHS.BOARD);
+    this.tileContainer.setSize(tileBack.width, tileFront.height);
+
+    this.doorSprite = scene.add
+      .sprite(worldX, worldY, "all-assets", `${doorPrefix}-0`)
       .setOrigin(0, 0.5)
-      .setDepth(DEPTHS.BOARD);
-    this.sprite.setPosition(worldX - this.sprite.width / 2, worldY);
+      .setDepth(DEPTHS.DIALOGUE);
 
     scene.anims.create({
       key: `${doorPrefix}-open`,
@@ -39,6 +54,8 @@ export default class Exit {
       frameRate: 24
     });
 
+    this.doorMagnifyEffect = new MagnifyEffect(scene, this.doorSprite, 1.0, 1.1, 100);
+    this.tileMagnifyEffect = new MagnifyEffect(scene, this.tileContainer, 1.0, 1.1, 100);
     this.enableInteractive();
   }
 
@@ -49,32 +66,40 @@ export default class Exit {
   open() {
     if (!this.isCurrentlyOpen) {
       this.isCurrentlyOpen = true;
-      this.sprite.play(`${this.doorPrefix}-open`);
+      this.doorSprite.play(`${this.doorPrefix}-open`);
     }
   }
 
   close() {
     if (this.isCurrentlyOpen) {
       this.isCurrentlyOpen = false;
-      this.sprite.play(`${this.doorPrefix}-close`);
+      this.doorSprite.play(`${this.doorPrefix}-close`);
     }
   }
 
   enableInteractive() {
-    this.sprite.setInteractive();
-    this.sprite.on("pointerover", this.onHoverStart);
-    this.sprite.on("pointerout", this.onHoverEnd);
-    this.sprite.on("pointerdown", this.onPointerDown);
+    this.doorSprite.setInteractive();
+    this.doorSprite.on("pointerover", this.onHoverStart);
+    this.doorSprite.on("pointerout", this.onHoverEnd);
+    this.doorSprite.on("pointerdown", this.onPointerDown);
+    this.tileContainer.setInteractive();
+    this.tileContainer.on("pointerover", this.onHoverStart);
+    this.tileContainer.on("pointerout", this.onHoverEnd);
+    this.tileContainer.on("pointerdown", this.onPointerDown);
   }
 
   disableInteractive() {
     // NOTE(rex): This handles the edge case of onHoverEnd never triggering, since the gameManager
     // disables interactivity when the move action kicks off.
     this.onHoverEnd();
-    this.sprite.disableInteractive();
-    this.sprite.off("pointerover", this.onHoverStart);
-    this.sprite.off("pointerout", this.onHoverEnd);
-    this.sprite.off("pointerdown", this.onPointerDown);
+    this.doorSprite.disableInteractive();
+    this.doorSprite.off("pointerover", this.onHoverStart);
+    this.doorSprite.off("pointerout", this.onHoverEnd);
+    this.doorSprite.off("pointerdown", this.onPointerDown);
+    this.tileContainer.disableInteractive();
+    this.tileContainer.off("pointerover", this.onHoverStart);
+    this.tileContainer.off("pointerout", this.onHoverEnd);
+    this.tileContainer.off("pointerdown", this.onPointerDown);
   }
 
   onPointerDown = (pointer: Phaser.Input.Pointer) => {
@@ -84,31 +109,22 @@ export default class Exit {
   };
 
   onHoverStart = () => {
-    if (this.tween) this.tween.stop();
-    this.tween = this.scene.add.tween({
-      targets: this.sprite,
-      scaleX: 1.1,
-      scaleY: 1.1,
-      duration: 100
-    });
+    this.doorMagnifyEffect.scaleUp();
+    this.tileMagnifyEffect.scaleUp();
     this.levelEvents.emit(EVENTS.EXIT_OVER, this);
   };
 
   onHoverEnd = () => {
-    if (this.tween) this.tween.stop();
-    this.tween = this.scene.add.tween({
-      targets: this.sprite,
-      scaleX: 1,
-      scaleY: 1,
-      duration: 100
-    });
+    this.doorMagnifyEffect.scaleDown();
+    this.tileMagnifyEffect.scaleDown();
     this.levelEvents.emit(EVENTS.EXIT_OUT, this);
   };
 
   destroy() {
     this.disableInteractive();
-    if (this.tween) this.tween.stop();
+    this.doorMagnifyEffect.destroy();
+    this.tileMagnifyEffect.destroy();
     this.scene = undefined;
-    this.sprite.destroy();
+    this.doorSprite.destroy();
   }
 }
