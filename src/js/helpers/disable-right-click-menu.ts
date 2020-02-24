7460b5ffafedcb11ff680e2969f9ac9ed0b12b0a
Michael Hadley <michaelwesthadley@gmail.com>
2020-02-23 19:00:06 -0600

    Disable right click to TS

--- src/js/helpers/disable-right-click-menu.ts
+++ src/js/helpers/disable-right-click-menu.ts
@@ -1,4 +1,4 @@
-export default function disableRightClickMenu(selector) {
+export default function disableRightClickMenu(selector: string) {
   document.querySelector(selector).addEventListener("contextmenu", e => {
     e.preventDefault();
     return false;
