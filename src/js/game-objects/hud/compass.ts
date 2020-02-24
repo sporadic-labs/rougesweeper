89ba542ba6b78389d894b175d026ababe420f8ed
rex <retwedt@gmail.com>
2020-02-23 18:19:08 -0600

    Covert compass to typescript.  When the player collects the key, switch the compass to point to the exit.

--- src/js/game-objects/hud/compass.ts
+++ src/js/game-objects/hud/compass.ts
@@ -1,16 +1,28 @@
-import { autorun } from "mobx";
 import { Math as PMath } from "phaser";
 import EventProxy from "../../helpers/event-proxy";
+import MobXProxy from "../../helpers/mobx-proxy";
 import DEPTHS from "../depths";
+import Level from "../level/level";
+import Player from "../player";
+import store from "../../store";
+
+enum COMPASS_TARGETS {
+  KEY = "KEY",
+  DOOR = "DOOR"
+}
 
 export default class Compass {
+  private sprite: Phaser.GameObjects.Sprite;
+  private playerOffset: number;
+  private angleOffset: number;
+  private target: COMPASS_TARGETS;
+  private mobProxy: MobXProxy;
+  private proxy: EventProxy;
+
   /**
    * @param {Phaser.Scene} scene
    */
-  constructor(scene, player, level) {
-    this.scene = scene;
-    this.player = player;
-    this.level = level;
+  constructor(private scene: Phaser.Scene, private player: Player, private level: Level) {
     this.playerOffset = 75;
     this.angleOffset = Math.PI / 2; // Graphic points up, so it's off by 90 deg
 
@@ -19,6 +31,15 @@ export default class Compass {
       .setDepth(DEPTHS.ABOVE_PLAYER)
       .setAlpha(0.95);
 
+    if (store.hasKey) this.target = COMPASS_TARGETS.DOOR;
+    else this.target = COMPASS_TARGETS.KEY;
+
+    this.mobProxy = new MobXProxy();
+    this.mobProxy.observe(store, "hasKey", () => {
+      if (store.hasKey) this.target = COMPASS_TARGETS.DOOR;
+      else this.target = COMPASS_TARGETS.KEY;
+    });
+
     this.proxy = new EventProxy();
     this.proxy.on(scene.events, "update", this.update, this);
     this.proxy.on(scene.events, "shutdown", this.destroy, this);
@@ -26,7 +47,10 @@ export default class Compass {
   }
 
   update() {
-    const target = this.level.getExitWorldPosition();
+    let target;
+    if (this.target === COMPASS_TARGETS.KEY) target = this.level.getKeyWorldPosition();
+    else if (this.target === COMPASS_TARGETS.DOOR) target = this.level.getExitWorldPosition();
+    if (!target) return;
     const center = this.level.gridXYToWorldXY(this.player.getGridPosition());
     const angle = PMath.Angle.BetweenPoints(center, target);
     this.sprite.rotation = angle + this.angleOffset;
