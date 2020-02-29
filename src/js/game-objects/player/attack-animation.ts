4cb5434a55b4bcad34b558932d18c55233d5b63b
Michael Hadley <michaelwesthadley@gmail.com>
2020-02-29 15:12:38 -0600

    Convert to AttackAnimation to TS

--- src/js/game-objects/player/attack-animation.ts
+++ src/js/game-objects/player/attack-animation.ts
@@ -1,47 +1,47 @@
 import DEPTHS from "../depths";
+import { Scene, GameObjects, Tweens, Types } from "phaser";
 
 export default class AttackAnimation {
-  /**
-   * @param {Phaser.Scene} scene
-   * @param {string} key
-   * @param {number} x
-   * @param {number} y
-   */
-  constructor(scene, key, x, y) {
+  private sprite: GameObjects.Sprite;
+  private tween: Tweens.Tween;
+
+  constructor(private scene: Scene, key: string, x: number, y: number) {
     this.scene = scene;
 
     this.sprite = scene.add.sprite(0, 0, "all-assets", key).setOrigin(0.5, 0.5);
     this.sprite.setDepth(DEPTHS.ABOVE_PLAYER);
     this.sprite.setVisible(false);
+    this.sprite.setAlpha(0);
 
     this.setPosition(x, y);
   }
 
-  fadeout(delay = 0) {
+  fadeout(delay = 0): Promise<void> {
     setTimeout(() => {
       this.sprite.setVisible(true);
     }, delay);
-    return new Promise((resolve, reject) => {
-      this.scene.tweens.add({
+    return new Promise(resolve => {
+      this.tween?.stop();
+      this.tween = this.scene.tweens.add({
         targets: this.sprite,
         alpha: 0,
         duration: 600,
         delay: delay,
         ease: "Quad.easeOut",
-        onComplete: resolve,
+        onComplete: () => resolve,
         callbackScope: this
       });
     });
   }
 
-  setPosition(x, y) {
+  setPosition(x: number, y: number) {
     const cx = x + this.sprite.width / 2;
     this.sprite.x = cx;
     this.sprite.y = y;
   }
 
   destroy() {
-    this.scene.tweens.killTweensOf(this.text);
+    this.tween?.stop();
     this.sprite.destroy();
   }
 }
