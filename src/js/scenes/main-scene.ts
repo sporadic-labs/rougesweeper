cd698d55d7bd92f5b49d201c85a40ca5e2dc3a58
Mike Hadley <mikewesthad@gmail.com>
2022-11-12 15:17:25 -0600

    Remove title

--- src/js/scenes/main-scene.ts
+++ src/js/scenes/main-scene.ts
@@ -8,28 +8,14 @@ import LevelIndicator from "../game-objects/hud/level-indicator";
 import store from "../store/index";
 import ToastManager from "../game-objects/hud/toast-manager";
 import ItemSwitcher from "../game-objects/hud/item-switcher";
-import constants from "../constants";
-
-const titleStyle = {
-  align: "left",
-  color: constants.lightText,
-  fontSize: "30px",
-  fontStyle: "bold",
-};
 
 export default class MainScene extends Scene {
   create() {
     store.startNewGame();
 
-    const width = Number(this.game.config.width);
-    const height = Number(this.game.config.height);
-
     const player = new Player(this, 0, 0);
     const toastManager = new ToastManager(this);
-    const gameManager = new GameManager(this, player, toastManager);
-    const gameTitle = this.add
-      .text(width * 0.9, height * 0.46, "SpyWare\n(or whatever...)", titleStyle)
-      .setOrigin(0.5, 0);
+    new GameManager(this, player, toastManager);
 
     new TechIndicator(this, store);
     new AlertIndicator(this, store);
